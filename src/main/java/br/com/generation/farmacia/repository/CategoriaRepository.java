2b38ba9308b350ca3781f0e09863f3924ce2c781
Edgard Henrique <edigarhenriqu@gmail.com>
2022-06-04 18:16:13 -0300

    Farmacia

---
+++ src/main/java/br/com/generation/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package br.com.generation.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.generation.farmacia.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("tipo") String nome);
+
+}
